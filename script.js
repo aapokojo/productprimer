879d7fe437973d2d2fd14d59464d17e26f5a6f71
Aapo Kojo <aapo@aapokojo.com>
2025-02-08 11:45:28 +0200

    separate pages to separate files

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ function initHtml() {
     document.body.insertAdjacentHTML('afterbegin', '<div class="overlay" id="overlay" onclick="toggleMenu()"></div>');
     
     document.getElementById("toc").insertAdjacentHTML('afterbegin', 
-        '<h3><a href="#starterguide" onclick="toggleMenu()">Product Primer</a></h3>'
+        '<h3><a href="index.html#starterguide" onclick="toggleMenu()">Product Primer</a></h3>'
         + '<h4>Contents</h4>'
-        + '<div><a href="#introduction" onclick="toggleMenu()">Introduction</a></div>'
-        + '<div><a href="#abouttheauthor" onclick="toggleMenu()">About the Author</a></div>'
+        + '<div><a href="index.html#introduction" onclick="toggleMenu()">Introduction</a></div>'
+        + '<div><a href="index.html#abouttheauthor" onclick="toggleMenu()">About the Author</a></div>'
         + '<hr/>'
-        + '<h3><a href="#purpose" onclick="toggleMenu()">What is the purpose of the product?</a></h3>'
+        + '<h3><a href="purpose.html#purpose" onclick="toggleMenu()">What is the purpose of the product?</a></h3>'
         + '<h4>Finding the key value proposition</h4>'
-        + '<div><a href="#users" onclick="toggleMenu()">From user needs to market understanding</a></div>'
-        + '<div><a href="#uniquvalueprop" onclick="toggleMenu()">Finding the Unique Value Proposition</a></div>'
-        + '<div><a href="#keytakeawayspurpose" onclick="toggleMenu()">Key Takeaways</a></div>'
+        + '<div><a href="purpose.html#users" onclick="toggleMenu()">From user needs to market understanding</a></div>'
+        + '<div><a href="purpose.html#uniquvalueprop" onclick="toggleMenu()">Finding the Unique Value Proposition</a></div>'
+        + '<div><a href="purpose.html#keytakeawayspurpose" onclick="toggleMenu()">Key Takeaways</a></div>'
         + '<hr/>'
     );
 
