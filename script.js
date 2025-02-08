866e395378b6152df96945ca106d7dc9d678494c
Aapo Kojo <aapo@aapokojo.com>
2025-02-08 11:41:10 +0200

    menu building in script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+function initHtml() {
+    document.body.insertAdjacentHTML('afterbegin', '<button id="menu-button" onclick="toggleMenu()">☰</button>');
+    document.body.insertAdjacentHTML('afterbegin', '<div class="overlay" id="overlay" onclick="toggleMenu()"></div>');
+    
+    document.getElementById("toc").insertAdjacentHTML('afterbegin', 
+        '<h3><a href="#starterguide" onclick="toggleMenu()">Product Primer</a></h3>'
+        + '<h4>Contents</h4>'
+        + '<div><a href="#introduction" onclick="toggleMenu()">Introduction</a></div>'
+        + '<div><a href="#abouttheauthor" onclick="toggleMenu()">About the Author</a></div>'
+        + '<hr/>'
+        + '<h3><a href="#purpose" onclick="toggleMenu()">What is the purpose of the product?</a></h3>'
+        + '<h4>Finding the key value proposition</h4>'
+        + '<div><a href="#users" onclick="toggleMenu()">From user needs to market understanding</a></div>'
+        + '<div><a href="#uniquvalueprop" onclick="toggleMenu()">Finding the Unique Value Proposition</a></div>'
+        + '<div><a href="#keytakeawayspurpose" onclick="toggleMenu()">Key Takeaways</a></div>'
+        + '<hr/>'
+    );
+
+    document.body.insertAdjacentHTML('beforeend', '<div class="footer">Product Primer | <a href="mailto:aapo@productprimer.com">aapo@productprimer.com</a></div>');
+}
+
+function toggleMenu() {
+    var toc = document.getElementById("toc");
+    var overlay = document.getElementById("overlay");
+    var body = document.body;
+    if (toc.style.left === "0px") {
+        toc.style.left = "-250px";
+        overlay.style.display = "none";
+        body.style.overflow = "";
+    } else {
+        toc.style.left = "0px";
+        overlay.style.display = "block";
+        body.style.overflow = "hidden";
+    }
+}
